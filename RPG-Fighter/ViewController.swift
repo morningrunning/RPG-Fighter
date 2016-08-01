008d4551bfaa0f4ace1969fba897b437ff87fc7b
“morningrunning” <rmorgan01@gmail.com>
2016-07-31 21:15:06 -0400

    Initial Commit

---
+++ RPG-Fighter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPG-Fighter
+//
+//  Created by Ronald Morgan on 7/31/16.
+//  Copyright © 2016 Ronald Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
